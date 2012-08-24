d5d8f6254f67f6f4b6f40f8e5e944c4fd1dee9eb
Dan Carley <dan.carley@gmail.com>
2012-08-24 07:55:38 +0100

    Add tests
    Tests for the following:
    
    - EL, Debian and Ubuntu package names.
    - Unsupported OS exceptions.
    - EPEL includes on EL - required pre_condition class.
    
    Largely exercises the variables that come from the params class.

---
+++ spec/classes/erlang_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+describe 'erlang' do
+  describe 'when osfamily is RedHat' do
+    let(:facts) {{
+      :osfamily => 'RedHat',
+    }}
+    # Mock EPEL class for include.
+    let :pre_condition do
+      'class epel {}'
+    end
+
+    it { should include_class('epel') }
+    it { should contain_package('erlang').with(
+      :name     => 'erlang',
+      :ensure   => 'present',
+      :require  => 'Class[Epel]'
+    )}
+  end
+
+  describe 'when osfamily is Debian' do
+    let(:facts) {{
+      :osfamily => 'Debian',
+    }}
+
+    it { should contain_package('erlang').with(
+      :name     => 'erlang-nox',
+      :ensure   => 'present',
+      :require  => nil,
+    )}
+  end
+
+  describe 'when operatingsystem is Debian' do
+    let(:facts) {{
+      :osfamily         => nil,
+      :operatingsystem  => 'Debian',
+    }}
+
+    it { should contain_package('erlang').with(
+      :name     => 'erlang-nox',
+      :ensure   => 'present',
+      :require  => nil,
+    )}
+  end
+
+  describe 'when osfamily and operatingsystem are unsupported' do
+    let(:facts) {{
+      :osfamily         => 'foo',
+      :operatingsystem  => 'bar',
+    }}
+
+    it { expect{ subject }.to raise_error(
+      /^Module \S+ does not support bar/
+    )}
+  end
+end
